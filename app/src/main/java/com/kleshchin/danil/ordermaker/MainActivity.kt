0e0147cc26238483631f6766041ec189f8a7db84
Danil Kleshchin <dkleshchin@distillery.com>
2017-11-29 23:28:36 +0300

    Initial commit

---
+++ app/src/main/java/com/kleshchin/danil/ordermaker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kleshchin.danil.ordermaker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
