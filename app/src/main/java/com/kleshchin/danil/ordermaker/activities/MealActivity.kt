5dcbdc2e77e893a2453240962895f06c7b273748
Danil Kleshchin <danil.kleshchin@gmail.com>
2018-01-05 15:59:04 +0300

    Add basket to toolbar

--- app/src/main/java/com/kleshchin/danil/ordermaker/activities/MealActivity.kt
+++ app/src/main/java/com/kleshchin/danil/ordermaker/activities/MealActivity.kt
@@ -1,14 +1,17 @@
 package com.kleshchin.danil.ordermaker.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.kleshchin.danil.ordermaker.R
 import com.kleshchin.danil.ordermaker.adapters.MealAdapter
 import com.kleshchin.danil.ordermaker.models.Meal
 import kotlinx.android.synthetic.main.meal_activity.*
+
 
 class MealActivity : AppCompatActivity(), MealAdapter.MealViewHolder.OnMealCheckedChangeListener {
 
@@ -19,16 +22,22 @@ class MealActivity : AppCompatActivity(), MealAdapter.MealViewHolder.OnMealCheck
 
     init {
         val url = "https://cdn.pixabay.com/photo/2015/06/19/23/04/spaghetti-815385_960_720.jpg"
+        val sushi = "http://www.xarakiri.ru/upload/iblock/97f/97f8f455544c87b3d1ab5b0079fb16f1.jpg"
+        val wok = "https://www.makitao.ru/userfiles/menu/big_butaniku_ydon_lapsha_wok_krasnodar_envdtf9.jpg"
+        val pizza = "http://supercook.ru/images-pizza/page-pizza-pastry-04.jpg"
+        val funchoza = "https://i.ytimg.com/vi/TVaCQs5rS6c/maxresdefault.jpg"
+        val shaurma = "https://www.edimdoma.ru/data/ckeditor_pictures/27454/content_fotolia_119231708_subscription_xxl.jpg"
+        meals.add(Meal("Суши", sushi, "300 руб", false))
+        meals.add(Meal("Вок", wok, "500 руб", false))
+        meals.add(Meal("Фунчоза", funchoza, "200 руб", false))
+        meals.add(Meal("Пицца", pizza, "400 руб", false))
+        meals.add(Meal("Шаурма", shaurma, "350 руб", false))
         meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
-        meals.add(Meal("Паста", url, "100 руб", false))
+        meals.add(Meal("Суши", sushi, "300 руб", false))
+        meals.add(Meal("Вок", wok, "500 руб", false))
+        meals.add(Meal("Фунчоза", funchoza, "200 руб", false))
+        meals.add(Meal("Пицца", pizza, "400 руб", false))
+        meals.add(Meal("Шаурма", shaurma, "350 руб", false))
         meals.add(Meal("Паста", url, "100 руб", false))
     }
 
@@ -62,6 +71,24 @@ class MealActivity : AppCompatActivity(), MealAdapter.MealViewHolder.OnMealCheck
         } else {
             checkedMeals.remove(meal)
         }
-        Log.d("AHA", checkedMeals.size.toString())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.meal_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        when(menuItem.itemId) {
+            android.R.id.home -> finish()
+            R.id.basket -> onBasketClick()
+        }
+        return super.onOptionsItemSelected(menuItem)
+    }
+
+    private fun onBasketClick() {
+        val intent: Intent = Intent(this, OrderActivity::class.java)
+        startActivity(intent)
     }
 }
