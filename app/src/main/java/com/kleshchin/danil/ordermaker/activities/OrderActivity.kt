985becdad3df8c8d6f162548d7357b054b37a6fc
Danil Kleshchin <danil.kleshchin@gmail.com>
2018-01-28 19:06:33 +0300

    Implement Order activity

--- app/src/main/java/com/kleshchin/danil/ordermaker/activities/OrderActivity.kt
+++ app/src/main/java/com/kleshchin/danil/ordermaker/activities/OrderActivity.kt
@@ -1,13 +1,171 @@
 package com.kleshchin.danil.ordermaker.activities
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.view.MenuItem
 import com.kleshchin.danil.ordermaker.R
+import com.kleshchin.danil.ordermaker.adapters.OrderAdapter
+import com.kleshchin.danil.ordermaker.models.Meal
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.view.View
+import kotlinx.android.synthetic.main.order_activity.*
+import java.util.*
 
-class OrderActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity(), View.OnClickListener {
+    private val notificationId = 123
+    private var meals: ArrayList<Meal> = arrayListOf()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: OrderAdapter
+    private var totalAmount = 0
+
+    companion object {
+        private val MEAL_ARRAY_KEY = "MEAL_ARRAY_KEY"
+
+        fun getOrderIntent(context: Context, meal: ArrayList<Meal>?): Intent {
+            val intent = Intent(context, OrderActivity::class.java)
+            return intent.putExtra(MEAL_ARRAY_KEY, meal)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.order_activity)
+
+        setSupportActionBar(order_toolbar as Toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeButtonEnabled(true)
+            actionBar.setDisplayShowTitleEnabled(false)
+        }
+
+        button_place_order.setOnClickListener(this)
+        meals = intent.getParcelableArrayListExtra(MEAL_ARRAY_KEY)
+        adapter = OrderAdapter(meals)
+        linearLayoutManager = LinearLayoutManager(this)
+        order_recycler_view.layoutManager = this.linearLayoutManager
+        order_recycler_view.adapter = adapter
+        val swipeHandler = object : SwipeToDeleteCallback(this) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = order_recycler_view.adapter as OrderAdapter
+                totalAmount -= meals[viewHolder.adapterPosition].mealPrice
+                adapter.removeAt(viewHolder.adapterPosition)
+                order_total.text = String.format(resources.getString(R.string.order_total), totalAmount)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(order_recycler_view)
+
+        totalAmount = meals.sumBy { it.mealPrice }
+        order_total.text = String.format(resources.getString(R.string.order_total), totalAmount)
     }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            android.R.id.home -> {
+                cancelNotification()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(menuItem)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0) {
+            button_place_order -> {
+                sendOrderToServer()
+                createStatusNotification()
+                finish()
+            }
+        }
+    }
+
+    private fun sendOrderToServer() {
+
+    }
+
+    private fun createStatusNotification() {
+        if (totalAmount == 0) {
+            return
+        }
+
+        var intent = Intent()
+        var pendingIntent = PendingIntent.getActivity(this@OrderActivity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        var notificationBuilder =
+                Notification.Builder(this@OrderActivity)
+                        .setContentIntent(pendingIntent)
+                        .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                        .setLargeIcon(BitmapFactory.decodeResource(this@OrderActivity.resources, R.mipmap.ocaka_logo_icon))
+                        .setWhen(System.currentTimeMillis())
+                        .setAutoCancel(false)
+
+                        .setContentTitle("Статус заказа")
+                        .setStyle(Notification.BigTextStyle().bigText("Ваш заказ находится в очереди на приготовление. Ожидайте"))
+                        .setOngoing(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            notificationBuilder.setShowWhen(true)
+        }
+        var notification = notificationBuilder.build()
+        notification.defaults = Notification.DEFAULT_SOUND
+        notification.defaults = Notification.DEFAULT_VIBRATE
+        var notificationManager: NotificationManager = this@OrderActivity.getSystemService(android.content.Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, notification)
+    }
+
+    private fun cancelNotification() {
+        var notificationManager: NotificationManager = this@OrderActivity.getSystemService(android.content.Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(notificationId)
+    }
+
+    abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+        private val deleteIcon = ContextCompat.getDrawable(context, R.drawable.delete_icon)
+        private val intrinsicWidth = deleteIcon.intrinsicWidth
+        private val intrinsicHeight = deleteIcon.intrinsicHeight
+        private val background = ColorDrawable()
+        private val backgroundColor = Color.parseColor("#f44336")
+
+        override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+            return false
+        }
+
+        override fun onChildDraw(c: Canvas?, recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+            val itemView = viewHolder.itemView
+            val itemHeight = itemView.bottom - itemView.top
+
+            // Draw the red delete background
+            background.color = backgroundColor
+            background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+            background.draw(c)
+
+            // Calculate position of delete icon
+            val deleteIconTop = itemView.top + (itemHeight - intrinsicHeight) / 2
+            val deleteIconMargin = (itemHeight - intrinsicHeight) / 2
+            val deleteIconLeft = itemView.right - deleteIconMargin - intrinsicWidth
+            val deleteIconRight = itemView.right - deleteIconMargin
+            val deleteIconBottom = deleteIconTop + intrinsicHeight
+
+            // Draw the delete icon
+            deleteIcon.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom)
+            deleteIcon.draw(c)
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/kleshchin/danil/ordermaker/adapters/OrderAdapter.kt
@@ -0,0 +1,65 @@
+package com.kleshchin.danil.ordermaker.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.kleshchin.danil.ordermaker.R
+import com.kleshchin.danil.ordermaker.activities.MealInfoActivity
+import com.kleshchin.danil.ordermaker.models.Meal
+import com.kleshchin.danil.ordermaker.utilities.inflate
+import kotlinx.android.synthetic.main.item_order_recycler_view.view.*
+
+class OrderAdapter(private val mealList: ArrayList<Meal>?) : RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
+
+    companion object {
+        private val MEAL_KEY = "MEAL"
+    }
+
+    override fun getItemCount(): Int {
+        if (mealList != null) {
+            return mealList.size
+        }
+        return 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
+        val inflatedView = parent.inflate(R.layout.item_order_recycler_view, false)
+        return OrderViewHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
+        if (mealList == null) {
+            return
+        }
+        val itemMeal = mealList[position]
+        holder.bindMeal(itemMeal)
+    }
+
+    fun removeAt(position: Int) {
+        mealList?.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    class OrderViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var view: View = v
+        private var meal: Meal? = null
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            val context = itemView.context
+            val categoryIntent = MealInfoActivity.getMealInfoIntent(v.context, this.meal)
+            categoryIntent.putExtra(MEAL_KEY, meal)
+            context.startActivity(categoryIntent)
+        }
+
+        fun bindMeal(meal: Meal) {
+            this.meal = meal
+            view.order_meal_name.text = meal.mealName
+            view.order_meal_price.text = String.format(view.context.resources.getString(R.string.meal_price), meal.mealPrice)
+            view.order_position.text = (adapterPosition + 1).toString()
+        }
+    }
+}
